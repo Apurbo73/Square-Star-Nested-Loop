2095894c9d1ba3be062152e803314ad49faa0531
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-29 00:48:35 +0600

    done

--- a.cpp
+++ a.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 1; i <= n; i++) { // Outer Loop
+        for (int j = 1; j <= n; j++) { // Inner Loop
             cout << "*" << " ";
         }
         cout << endl;
